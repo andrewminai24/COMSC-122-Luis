8db40d2e969c6248622692fc4df3f51ce588d935
Andrewminai24 <andrewminai24@gmail.com>
2019-12-18 14:01:57 -0800

    Hw08 displays characters

--- Hw08/Main.py
+++ Hw08/Main.py
@@ -1,33 +1,57 @@
-def main():
-#local variables
-num_upper = 0
-num_lower = 0
-num_space = 0
-num_digits = 0
-data = ''
-#open file text.txt for reading
-infile = open('text.txt', 'r')
-# read in data from the file
-data = infile.read()
-# Step through each character in the file
-#Determine if the character is uppercase
-#lowercase, a digit or space and keep a
-#running total of each
-for ch in data:
-if ch.isupper():
-num_upper = num_upper + 1
-if ch.islower():
-num_lower = num_lower + 1
-if ch.isdigit():
-num_digits = num_digits + 1
-if ch.isspace():
-num_space = num_space + 1
-#Close the file
-infile.close()
-#Display the totals
-print('The number of uppercase letters is:', num_upper)
-print('The number of lowercase letters is:', num_lower)
-print('The number of digits is:', num_digits)
-print('The number if whitespace is:', num_space)
-# call the main function
-main()
+print("luis")
+
+ifs = open("text.txt",'r')
+
+lines = ifs.readlines()
+
+ifs.close()
+
+upper = 0
+
+lower = 0
+white = 0
+digit = 0
+nonalpha = 0
+idx = 0
+while idx < len(lines):
+    for ch in lines[idx]:
+        if ch.islower():
+            lower = lower + 1;
+        if ch.isupper():
+            upper = upper + 1;
+        if ch.isdigit():
+            digit = digit + 1;
+        if ch.isspace():
+            white = white + 1;
+        if not ch.isalnum() :
+            nonalpha = nonalpha + 1;
+    idx = idx+1
+
+print (" upper : ", upper)
+print (" lower : ", lower)
+print (" digit : ", digit)
+print (" white : ", white)
+print (" nonalphanumeric : ", nonalpha)
+
+
+print("Luis")
+
+ifs = open("text.txt",'r')
+
+lines = ifs.readlines()
+
+ifs.close()
+
+print (lines)
+
+idx = 0
+res = 0
+wordarr = []
+while idx < len(lines):
+    wordarr = lines[idx].split()
+    for word in wordarr:
+        if word != "--":
+          res = res + 1;
+    idx = idx + 1
+
+print ( " Number of words : " , res )
